065f8245db24245367bde1dfdbcc33e228d0eddd
patricio <patoare@hotmail.com>
2025-01-06 17:46:39 +0100

    a bit of style

--- src/pages/AllPetsPage.jsx
+++ src/pages/AllPetsPage.jsx
@@ -1,5 +1,7 @@
+import "../styles/AllPetsPage.css"; 
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
 function AllAnimalsPage() {
   const [animals, setAnimals] = useState([]); // State to store fetched animals
   const [error, setError] = useState(null); // State for error handling
@@ -29,46 +31,31 @@ function AllAnimalsPage() {
   if (error) {
     return <p>Error: {error}</p>; // Show error message
   }
+
+
   return (
     <div className="container">
-      <h1 style={{ color: 'white' }}> List of All Animals</h1>
+
+      <h1 className="title"> List of All Animals</h1>
+
       <div className='animal-list'>
         {animals.length > 0 ? (
           animals.map((animal) => (
-            <div
-              key={animal.id}
-              style={{
-                border: '1px solid #ccc',
-                margin: '10px',
-                padding: '10px',
-                borderRadius: '5px',
-                backgroundColor: '#F9F9F',
-                textAlign: 'center',
-              }}
-            >
+            <div className="petBox"
+              key={animal.id} >
+
               <h3>{animal.name}</h3>
               <p>Type: {animal.type}</p>
               <p>Age: {animal.age}</p>
-              <img
+              <img className="photoBox"
                 src={animal.photos && animal.photos[0] ? animal.photos[0].small : 'defaultImage.jpg'}
                 alt="Pet Pic"
-                style={{ width: '150px', height: '150px', objectFit: 'cover' }}
               />
-              <div style={{ marginTop: '10px' }}>
-                <Link
-                  to={`/animals/${animal.id}`}
-                  style={{
-                    textDecoration: 'none',
-                    backgroundColor: '#007BFF',
-                    color: 'white',
-                    padding: '5px 10px',
-                    borderRadius: '5px',
-                  }}
-                >
-                  View Details
+               <Link to={`/animals/${animal.id}`}>
+                    <button  className="buttonBox" type="button" >Details</button>
                 </Link>
               </div>
-            </div>
+            
           ))
         ) : (
           <p>No animals available</p>
