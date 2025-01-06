aa1a6e0f1df3a9420c2c74468b49a55037b5d6b9
patricio <patoare@hotmail.com>
2025-01-06 13:27:23 +0100

    links to HomePage

--- src/pages/AllPetsPage.jsx
+++ src/pages/AllPetsPage.jsx
@@ -1,8 +1,14 @@
-
+import { Link } from "react-router-dom";
 function AllPetsPage () {
 return(
     <div>
-<h3>List of all pets from our site!</h3>
+ <h1 style={{color: "white" }}>List of all our Pets</h1>
+<Link to='/'>
+<div className="card" style={{ border: '2px solid black', textAlign: 'center', backgroundColor: "#591C21", color: "white", margin: "5px 10px 30px 5px" }}>
+        <h5>Back to Home Page</h5>
+        <button type="button">PetFinder MP</button>
+        </div>
+</Link>
 </div>
 )
 }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,15 @@
-
+import { Link } from "react-router-dom";
 function ContactPage() {
     return(
         <div>
-        <h1>This is the Contact Page</h1>
-        </div>
+        <h1 style={{color: "white" }}>Contact details</h1>
+       <Link to='/'>
+       <div className="card" style={{ border: '2px solid black', textAlign: 'center', backgroundColor: "#591C21", color: "white", margin: "5px 10px 30px 5px" }}>
+               <h5>Back to Home Page</h5>
+               <button type="button">PetFinder MP</button>
+               </div>
+       </Link>
+       </div>
     )
 }
 export default ContactPage;
